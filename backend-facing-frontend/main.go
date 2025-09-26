11afeee840dcbffa7b012c52a9521fc909dab62b
Arko Antony Mitra <arkoantonymitra@Arko-Antony-Mitra-B661s-MacBook-Pro.local>
2025-09-26 19:13:00 +0530

    Setup all services, using a shared util module for logging service names (All Services).

---
+++ backend-facing-frontend/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/aRKO872/ecommerce-product-admin/backend-facing-frontend/prepare"
+
+func main() {
+	prepare.Prepare()
+}
---
+++ logger-msc/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/aRKO872/ecommerce-product-admin/logger-msc/prepare"
+
+
+func main() {
+	prepare.Prepare()
+}
